76a21e1142ee8c0d7424ff46e5bb7e106d3d437a
yuna Kim <yuna000804@gmail.com>
2022-12-19 10:50:39 +0900

    fix : if 문 삭제

--- pages/shows/[...params].tsx
+++ pages/shows/[...params].tsx
@@ -13,15 +13,15 @@ export default function Detail() {
   const [click, setClick] = useState(false);
   const url = `https://image.tmdb.org/t/p/original${router.query.backdrop_path}`;
   const [mouseOver, letMouseOver] = useState(false);
-  if(typeof router.query.name !== "string") {
-    console.log("올바른 경로로 접속해주세요.");
-    return <></>
-  }
+  // if(typeof router.query.name !== "string") {
+  //   console.log("올바른 경로로 접속해주세요.");
+  //   return <></>
+  // }
   return(
     <div className={styles.bg} style={{backgroundImage: `url(${url})`}}>
       <div className={styles.titleAndPoster}>
       <div className={styles.title}>{router.query.name}</div>
-      <Seo title={router.query.name}></Seo>
+      {/* <Seo title={router.query.name}></Seo> */}
       <div className={styles.wrapper}>
         <div className={styles.posterWrap}>
           <img onMouseOver={() => {letMouseOver(true)}} onMouseOut={() => {letMouseOver(false)}} className={styles.poster} src={`https://image.tmdb.org/t/p/w500/${router.query.poster}`}>
